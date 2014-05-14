f83fdc11e6d66b3a7a61eb51508eb569a725fab9
村田 宗一朗 <Soichiro_Murata@voyagegroup.com>
2014-05-14 12:22:38 +0900

    Initial Commit

---
+++ PopDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PopDemo
+//
+//  Created by 村田 宗一朗 on 2014/05/14.
+//  Copyright (c) 2014年 murata gaia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PopDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PopDemo
+//
+//  Created by 村田 宗一朗 on 2014/05/14.
+//  Copyright (c) 2014年 murata gaia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
